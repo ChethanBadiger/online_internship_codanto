1670b7809cefddc2d2d667291806c617930eaf25
ChethanBadiger <chethanbadiger245@gmail.com>
2025-09-01 17:12:48 +0530

    completed cocktail project

--- week_3/app.js
+++ week_3/app.js
@@ -22,7 +22,7 @@ async function loadTable(){
         });
         
     } catch (error) {
-        console.error("bro there is a error: ",error);
+        console.error("bro there is a error in loading the table: ",error);
         
     }
 }
@@ -45,6 +45,31 @@ async function random_drink() {
             drink_table.innerHTML = row;
         });
     } catch (error) {
-        console.log("bro there is a error:", error)
+        console.log("bro there is a error in random drink:", error)
+    }
+}
+
+async function filter(category) {
+    try {
+        let rows = document.querySelectorAll('#drinks_table tbody tr');
+        let count = 0;
+
+        rows.forEach(row => {
+            let row_category = row.cells[2].textContent;
+
+            if(category === row_category){
+                row.style.display = '';
+                count += 1;
+            }
+            else{
+                 row.style.display = 'none';
+            }
+        });
+        
+        let lenghtOfDrinks = count;
+        document.getElementById('noOfData').textContent = `Showing ${lenghtOfDrinks} results`;
+    } catch (error) {
+
+        console.error("Bro there is a error in filtering:", error);
     }
 }
