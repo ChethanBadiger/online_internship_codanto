ea6d58590d4347b7958df5f64cca8426fe819a6d
ChethanBadiger <chethanbadiger245@gmail.com>
2025-09-04 19:44:40 +0530

    folder fix

--- week_4/reactjs practice/my-react-app/src/components/Bye.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Bye() {
-  return (
-    <h1>Hello from bye comonpont</h1>
-  )
-}
-
-export default Bye
--- week_4/reactjs practice/my-react-app/src/components/Hello.jsx
+++
@@ -1,7 +0,0 @@
-function Hello(params) {
-    return(
-        <h1>Hello from Hello component</h1>
-    )
-}
-
-export default Hello
