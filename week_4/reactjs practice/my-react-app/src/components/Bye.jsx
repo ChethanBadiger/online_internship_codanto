e0755cba012ca1a974c00eb13f1816891d8579ed
ChethanBadiger <chethanbadiger245@gmail.com>
2025-09-04 19:21:00 +0530

    react js components

---
+++ week_4/reactjs practice/my-react-app/src/components/Bye.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Bye() {
+  return (
+    <h1>Hello from bye comonpont</h1>
+  )
+}
+
+export default Bye
---
+++ week_4/reactjs practice/my-react-app/src/components/Hello.jsx
@@ -0,0 +1,7 @@
+function Hello(params) {
+    return(
+        <h1>Hello from Hello component</h1>
+    )
+}
+
+export default Hello
