3051854cd78aefdd20bad2699dd60bffafe228cc
ChethanBadiger <chethanbadiger245@gmail.com>
2025-09-08 19:43:00 +0530

    please fix hosting

--- week_4/Bill-splitter/vite.config.js
+++ week_4/Bill-splitter/vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/online_internship_codanto/week_4/Bill-splitter'
 })
