3132f77c8f394f5e347bcaa2d57341b495a2bcb7
ChethanBadiger <chethanbadiger245@gmail.com>
2025-09-11 21:10:03 +0530

    updated

---
+++ week_4/Bill-splitter/src/components/EnterManually.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function EnterManually() {
+  return (
+    <>
+      {/* fix the icon later */}
+      <p className="">
+        <i class="ri-arrow-left-line"></i> Back
+      </p>
+      <h1>Receipt Items</h1>
+      <p>List all the items on your receipt</p>
+      <form action=""></form>
+    </>
+  );
+}
+
+export default EnterManually;
---
+++ week_4/Bill-splitter/src/components/UploadReceipt.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function UploadReceipt() {
+  return (
+    <>
+    <h1>Upload your receipt</h1>
+    <p>Take a photo or upload an image of your receipt</p>
+
+    {/* image uploader */}
+
+    <button className={`bg-orange-600 py-3 px-30 rounded-lg mb-4 shadow-xl/20 text-white font-medium`}><>Scrape the Bill</></button>
+
+    </>
+  )
+}
+
+export default UploadReceipt
